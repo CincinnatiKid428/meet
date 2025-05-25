1b1cbc56bc3023b427b1062cfa0c7def77983268
CincinnatiKid428 <pweaver.psu03@gmail.com>
2025-05-24 23:26:49 -0400

    PWA update
    - Added npm packages: vite-plugin-pwa,  workbox-core, workbox-routing, workbox-precaching, workbox-strategies, workbox-expiration
    - vite.config.js: Added manifest entries
    - manifest.json: File added for PWA
    - service-working.js: File added for PWA
    - serviceWorkerRegistration.js: File added for PWA
    - main.jsx: Updated to import service worker and call .register()
    - index.html: Added <link> tag to manifest file

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration.js';
 
-import * as atatus from 'atatus-spa';
-atatus.config('909884598afd47f1a9001c30d43dc226').install();
+//import * as atatus from 'atatus-spa'; /*Atatus trial expires 5/25/2025*/
+//atatus.config('909884598afd47f1a9001c30d43dc226').install();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+serviceWorkerRegistration.register();
